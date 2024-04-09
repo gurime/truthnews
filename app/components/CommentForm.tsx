fc57f6072e1958d435f78c82ee4a808fa81ae534
Phillip Bailey <ztron500@gmail.com>
2024-04-08 20:20:40 -0400

    update

--- app/components/CommentForm.tsx
+++ app/components/CommentForm.tsx
@@ -3,7 +3,7 @@ import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import CommentList from './CommentList';
 import { addDoc, collection, doc, getDoc, getFirestore } from 'firebase/firestore';
-import { getAuth, Auth, User } from 'firebase/auth';
+import { getAuth, User } from 'firebase/auth';
 import { BeatLoader } from 'react-spinners';
 import { auth } from '../firebase/firebase';
 
@@ -21,177 +21,174 @@ articleId: string;
 }
 
 const CommentForm: React.FC<CommentFormProps> = ({ articleId }) => {
-  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
-  const [content, setContent] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [comments, setComments] = useState<Comment[]>([]);
-  const [successMessage, setSuccessMessage] = useState<string>("");
-  const [names, setNames] = useState<string[]>([]);
-  const [autoFocus, setAutoFocus] = useState<boolean>(true);
-  const [errorMessage, setErrorMessage] = useState<string>('');
+const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
+const [content, setContent] = useState<string>("");
+const [isLoading, setIsLoading] = useState<boolean>(false);
+const [comments, setComments] = useState<Comment[]>([]);
+const [successMessage, setSuccessMessage] = useState<string>("");
+const [names, setNames] = useState<string[]>([]);
+const [autoFocus, setAutoFocus] = useState<boolean>(true);
+const [errorMessage, setErrorMessage] = useState<string>('');
+const router = useRouter();
+useEffect(() => {
+const unsubscribe = auth.onAuthStateChanged(async (user: User | null) => {
+const getUserData = async (userId: string) => {
+try {
+const db = getFirestore();
+const userDocRef = doc(db, 'users', userId);
+const userDocSnapshot = await getDoc(userDocRef);
+if (userDocSnapshot.exists()) {
+const userData = userDocSnapshot.data();
+return userData;
+} else {
+return null;
+}
+} catch (error) {
+throw error;
+}
+};
+setIsSignedIn(!!user);
+if (user) {
+try {
+const userData = await getUserData(user.uid);
+if (userData) {
+setNames([userData.firstName, userData.lastName]);
+}
+} catch (error: any) {
+handleError(error);
+} finally {
+setIsLoading(false);
+}
+}
+});
+return () => unsubscribe();
+}, []);
 
-  const router = useRouter();
+const handleError = (error: { code: string }) => {
+if (error.code === 'network-error') {
+setErrorMessage('Network error: Please check your internet connection.');
+} else if (error.code === 'invalid-content') {
+setErrorMessage('Invalid comment content. Please try again.');
+} else {
+setErrorMessage('Unexpected error occurred. Please try again later.');
+}
+};
 
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(async (user: User | null) => {
-      const getUserData = async (userId: string) => {
-        try {
-          const db = getFirestore();
-          const userDocRef = doc(db, 'users', userId);
-          const userDocSnapshot = await getDoc(userDocRef);
-          if (userDocSnapshot.exists()) {
-            const userData = userDocSnapshot.data();
-            return userData;
-          } else {
-            return null;
-          }
-        } catch (error) {
-          throw error;
-        }
-      };
-      setIsSignedIn(!!user);
-      if (user) {
-        try {
-          const userData = await getUserData(user.uid);
-          if (userData) {
-            setNames([userData.firstName, userData.lastName]);
-          }
-        } catch (error: any) {
-          handleError(error);
-        } finally {
-          setIsLoading(false);
-        }
-      }
-    });
-    return () => unsubscribe();
-  }, []);
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+e.preventDefault();
+try {
+const auth = getAuth();
+const user = auth.currentUser;
+setIsLoading(true);
+const db = getFirestore();
+const docRef = await addDoc(collection(db, 'comments'), {
+articleId: articleId,
+userId: user?.uid,
+content: content,
+timestamp: new Date(),
+userName: user?.displayName,
+userEmail: user?.email,
+});
+setComments((prevComments) => [...prevComments,
+{
+id: docRef.id,
+userId: user?.uid,
+content: content,
+timestamp: new Date(),
+userName: user?.displayName,
+userEmail: user?.email,
+},
+]);
+setSuccessMessage('Comment created successfully');
+setTimeout(() => {
+setSuccessMessage('');
+}, 3000);
+setContent('');
+} catch (error) {
+setErrorMessage('Error submitting comment. Please try again.');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+} finally {
+setIsLoading(false);
+}
+};
 
-  const handleError = (error: { code: string }) => {
-    if (error.code === 'network-error') {
-      setErrorMessage('Network error: Please check your internet connection.');
-    } else if (error.code === 'invalid-content') {
-      setErrorMessage('Invalid comment content. Please try again.');
-    } else {
-      setErrorMessage('Unexpected error occurred. Please try again later.');
-    }
-  };
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    try {
-      const auth = getAuth();
-      const user = auth.currentUser;
-      setIsLoading(true);
-      const db = getFirestore();
-      const docRef = await addDoc(collection(db, 'comments'), {
-        articleId: articleId,
-        userId: user?.uid,
-        content: content,
-        timestamp: new Date(),
-        userName: user?.displayName,
-        userEmail: user?.email,
-      });
-      setComments((prevComments) => [...prevComments,
-      {
-        id: docRef.id,
-        userId: user?.uid,
-        content: content,
-        timestamp: new Date(),
-        userName: user?.displayName,
-        userEmail: user?.email,
-      },
-      ]);
-      setSuccessMessage('Comment created successfully');
-      setTimeout(() => {
-        setSuccessMessage('');
-      }, 3000);
-      setContent('');
-    } catch (error) {
-      setErrorMessage('Error submitting comment. Please try again.');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 3000);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  const handleLogout = async () => {
-    try {
-      await auth.signOut();
-      router.push('/pages/Login')
-    } catch (error) {
-      console.error(error);
-    }
-  };
+const handleLogout = async () => {
+try {
+await auth.signOut();
+router.push('/pages/Login')
+} catch (error) {
+console.error(error);
+}
+};
 
   return (
     <>
-      <form className="postform" onSubmit={handleSubmit}>
-        {isSignedIn ? (
-          <div className="commentreg-box">
-            {names.length === 2 && (
-              <>
-                <div className='navinfo-box'>
-                  <span className="navinfo">{names[0]}</span>
-                  <span className="navinfo">{names[1]}</span>
-                </div>
-              </>
-            )}
-            <button
-              style={{
-                width: 'auto',
-                marginBottom: '4px',
-              }}
-              type="button"
-              onClick={handleLogout}
-            >
-              Log out
-            </button>
-          </div>
-        ) : (
-          <div className="commentreg-box">
-            <button
-              style={{
-                backgroundColor: 'blue',
-                width: 'auto',
-                margin: '10px',
-              }}
-              onClick={() => router.push('/pages/Login')}>
-              Login
-            </button>
-            <button
-              style={{
-                margin: '10px',
-                width: 'auto',
-              }}
-              onClick={() => router.push('/pages/Register')}>
-              Register
-            </button>
-          </div>
-        )}
-        {/* post form start here here */}
-        <textarea
-          rows={5}
-          cols={50}
-          placeholder='Type Your Message'
-          required
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          autoFocus={autoFocus}></textarea>
-        <button
-          className={isSignedIn ? "submitbtn" : "submitbtn disabled"}
-          type="submit"
-          disabled={!isSignedIn || !content || isLoading}>
-          {isLoading ? <BeatLoader color='blue' /> : 'Comment'}
-        </button>
+<form className="postform" onSubmit={handleSubmit}>
+{isSignedIn ? (
+<div className="commentreg-box">
+{names.length === 2 && (
+<>
+<div className='navinfo-box'>
+<span className="navinfo">{names[0]}</span>
+<span className="navinfo">{names[1]}</span>
+</div>
+</>
+)}
+<button
+style={{
+width: 'auto',
+marginBottom: '4px',
+}}
+type="button"
+onClick={handleLogout}
+>
+Log out
+</button>
+</div>
+) : (
+<div className="commentreg-box">
+<button
+style={{
+backgroundColor: 'blue',
+width: 'auto',
+margin: '10px',
+}}
+onClick={() => router.push('/pages/Login')}>
+Login
+</button>
 
-      </form>
+<button
+style={{
+margin: '10px',
+width: 'auto',
+}}
+onClick={() => router.push('/pages/Register')}>
+Register
+</button>
+</div>
+)}
+{/* post form start here here */}
+<textarea
+rows={5}
+cols={50}
+placeholder='Type Your Message'
+required
+value={content}
+onChange={(e) => setContent(e.target.value)}
+autoFocus={autoFocus}></textarea>
 
-      <CommentList comments={comments} setComments={setComments} articleId={articleId} />
-
-    </>
-  )
+<button
+className={isSignedIn ? "submitbtn" : "submitbtn disabled"}
+type="submit"
+disabled={!isSignedIn || !content || isLoading}>
+{isLoading ? <BeatLoader color='blue' /> : 'Comment'}
+</button>
+</form>
+<CommentList comments={comments} setComments={setComments} articleId={articleId} />
+</>
+)
 }
 
 export default CommentForm;
--- app/components/CommentList.tsx
+++ app/components/CommentList.tsx
@@ -1,180 +1,177 @@
 'use client'
-import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { collection, deleteDoc, getDocs, getFirestore, doc, query, orderBy, where, getDoc, updateDoc } from 'firebase/firestore';
 import React, { useState, useEffect, useRef } from 'react';
 import { BeatLoader } from 'react-spinners';
 import EditCommentModal from './EditCommentModal';
 
 interface Comment {
-  id: string;
-  userId: string | undefined;
-  content: string;
-  timestamp: Date;
-  userName: string | null | undefined;
-  userEmail: string | null | undefined;
+id: string;
+userId: string | undefined;
+content: string;
+timestamp: Date;
+userName: string | null | undefined;
+userEmail: string | null | undefined;
 }
 
 interface CommentListProps {
-  comments: Comment[];
-  setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
-  articleId: string;
+comments: Comment[];
+setComments: React.Dispatch<React.SetStateAction<Comment[]>>;
+articleId: string;
 }
 
 const CommentList: React.FC<CommentListProps> = (props) => {
-  
-  const [errorMessage, setErrorMessage] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [editModalOpen, setEditModalOpen] = useState(false);
-  const [editingComment, setEditingComment] = useState<Comment | null>(null);
-  const [successMessage, setSuccessMessage] = useState<string | undefined>(undefined);
-  const { comments, setComments, articleId } = props;
-  const commentsRef = useRef<HTMLDivElement | null>(null);
+const [errorMessage, setErrorMessage] = useState('');
+const [loading, setLoading] = useState(true);
+const [editModalOpen, setEditModalOpen] = useState(false);
+const [editingComment, setEditingComment] = useState<Comment | null>(null);
+const [successMessage, setSuccessMessage] = useState<string | undefined>(undefined);
+const { comments, setComments, articleId } = props;
+const commentsRef = useRef<HTMLDivElement | null>(null);
 
-  const fetchComments = async (articleId: string) => {
-    try {
-      const db = getFirestore();
-      const commentsRef = collection(db, 'comments');
-      const queryRef = query(
-        commentsRef,
-        where(`articleId`, '==', articleId),
-        orderBy('timestamp', 'desc')
-      );
-      const querySnapshot = await getDocs(queryRef);
-      const newComments: Comment[] = querySnapshot.docs.map((doc) => {
-        const commentData = doc.data();
-        return {
-          id: doc.id,
-          userId: commentData.userId,
-          content: commentData.content,
-          timestamp: commentData.timestamp.toDate(),
-          userName: commentData.userName,
-          userEmail: commentData.userEmail,
-        };
-      });
-      setComments(newComments);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching comments:', error); // Add this line for logging
+const fetchComments = async (articleId: string) => {
+try {
+const db = getFirestore();
+const commentsRef = collection(db, 'comments');
+const queryRef = query(
+commentsRef,
+where(`articleId`, '==', articleId),
+orderBy('timestamp', 'desc')
+);
+const querySnapshot = await getDocs(queryRef);
+const newComments: Comment[] = querySnapshot.docs.map((doc) => {
+const commentData = doc.data();
+return {
+id: doc.id,
+userId: commentData.userId,
+content: commentData.content,
+timestamp: commentData.timestamp.toDate(),
+userName: commentData.userName,
+userEmail: commentData.userEmail,
+};
+});
+setComments(newComments);
+setLoading(false);
+} catch (error) {
+console.error('Error fetching comments:', error); 
+setErrorMessage('Error fetching comments. Please try again.');
+setLoading(false);
+}
+};
 
-      setErrorMessage('Error fetching comments. Please try again.');
-      setLoading(false);
-    }
-  };
+const userIsAuthenticated = async () => {
+return new Promise<boolean>((resolve) => {
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+const isAuthenticated = !!user;
+resolve(isAuthenticated);
+});
+});
+};
 
-  const userIsAuthenticated = async () => {
-    return new Promise<boolean>((resolve) => {
-      const auth = getAuth();
-      onAuthStateChanged(auth, (user) => {
-        const isAuthenticated = !!user;
-        resolve(isAuthenticated);
-      });
-    });
-  };
+const editPost = (postId: string, userId: string | undefined) => {
+const commentToEdit = comments.find((comment) => comment.id === postId);
+if (commentToEdit) {
+const auth = getAuth();
+const currentUser = auth.currentUser;
+if (currentUser && currentUser.uid === commentToEdit.userId) {
+setEditingComment(commentToEdit);
+setEditModalOpen(true);
+} else {
+setErrorMessage('Unauthorized to edit this comment.');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+} else {
+setErrorMessage('Comment not found');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+};
 
-  const editPost = (postId: string, userId: string | undefined) => {
-    const commentToEdit = comments.find((comment) => comment.id === postId);
-    if (commentToEdit) {
-      const auth = getAuth();
-      const currentUser = auth.currentUser;
-      if (currentUser && currentUser.uid === commentToEdit.userId) {
-        setEditingComment(commentToEdit);
-        setEditModalOpen(true);
-      } else {
-        setErrorMessage('Unauthorized to edit this comment.');
-        setTimeout(() => {
-          setErrorMessage('');
-        }, 3000);
-      }
-    } else {
-      setErrorMessage('Comment not found');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 3000);
-    }
-  };
+const handleEditModalSave = async (postId: string, editedContent: string) => {
+try {
+await updateComment(postId, editedContent);
+setEditModalOpen(false);
+} catch (error) {
+setErrorMessage('Error saving comment. Please try again.');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+};
 
-  const handleEditModalSave = async (postId: string, editedContent: string) => {
-    try {
-      await updateComment(postId, editedContent);
-      setEditModalOpen(false);
-    } catch (error) {
-      setErrorMessage('Error saving comment. Please try again.');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 3000);
-    }
-  };
+const handleEditModalCancel = () => {
+setEditModalOpen(false);
+};
 
-  const handleEditModalCancel = () => {
-    setEditModalOpen(false);
-  };
+const updateComment = async (postId: string, editedContent: string) => {
+try {
+const db = getFirestore();
+const commentRef = doc(db, 'comments', postId);await updateDoc(commentRef, {
+content: editedContent,
+});
+setComments((prevComments) =>
+prevComments.map((comment) => (comment.id === postId ? { ...comment, content: editedContent } : comment))
+);
+setSuccessMessage('Comment updated successfully');
+setTimeout(() => {
+setSuccessMessage(undefined);
+}, 3000);
+} catch (error) {
+setErrorMessage('Error updating comment. Please try again.');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+};
 
-  const updateComment = async (postId: string, editedContent: string) => {
-    try {
-      const db = getFirestore();
-      const commentRef = doc(db, 'comments', postId);
-      await updateDoc(commentRef, {
-        content: editedContent,
-      });
-      setComments((prevComments) =>
-        prevComments.map((comment) => (comment.id === postId ? { ...comment, content: editedContent } : comment))
-      );
-      setSuccessMessage('Comment updated successfully');
-      setTimeout(() => {
-        setSuccessMessage(undefined);
-      }, 3000);
-    } catch (error) {
-      setErrorMessage('Error updating comment. Please try again.');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 3000);
-    }
-  };
+const deletePost = async (postId: string, commentUserId: string | undefined) => {
+try {
+const auth = getAuth();
+const currentUser = auth.currentUser;
+const isAuthenticated = await userIsAuthenticated();
+if (currentUser) {
+if (currentUser.uid === commentUserId) {
+const db = getFirestore();
+const commentDoc = await getDoc(doc(db, 'comments', postId));
+if (commentDoc.exists()) {
+await deleteDoc(doc(db, 'comments', postId));
+setComments((prevComments) => prevComments.filter((comment) => comment.id !== postId));
+setSuccessMessage('Comment deleted successfully');
+setTimeout(() => {
+setSuccessMessage(undefined);
+}, 3000);
+} else {
+setErrorMessage('Comment not found');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+} else {
+setErrorMessage('Unauthorized to delete this comment.');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+}
+} catch (error) {
+setErrorMessage('Error deleting comment. Please try again.');
+setTimeout(() => {
+setErrorMessage('');
+}, 3000);
+}
+};
 
-  const deletePost = async (postId: string, commentUserId: string | undefined) => {
-    try {
-      const auth = getAuth();
-      const currentUser = auth.currentUser;
-      const isAuthenticated = await userIsAuthenticated();
-      if (currentUser) {
-        if (currentUser.uid === commentUserId) {
-          const db = getFirestore();
-          const commentDoc = await getDoc(doc(db, 'comments', postId));
-          if (commentDoc.exists()) {
-            await deleteDoc(doc(db, 'comments', postId));
-            setComments((prevComments) => prevComments.filter((comment) => comment.id !== postId));
-            setSuccessMessage('Comment deleted successfully');
-            setTimeout(() => {
-              setSuccessMessage(undefined);
-            }, 3000);
-          } else {
-            setErrorMessage('Comment not found');
-            setTimeout(() => {
-              setErrorMessage('');
-            }, 3000);
-          }
-        } else {
-          setErrorMessage('Unauthorized to delete this comment.');
-          setTimeout(() => {
-            setErrorMessage('');
-          }, 3000);
-        }
-      }
-    } catch (error) {
-      setErrorMessage('Error deleting comment. Please try again.');
-      setTimeout(() => {
-        setErrorMessage('');
-      }, 3000);
-    }
-  };
+useEffect(() => {
+setComments([]); // Reset comments to empty array
+fetchComments(articleId);
+}, [articleId]);
 
-  useEffect(() => {
-    setComments([]); // Reset comments to empty array
-    fetchComments(articleId);
-  }, [articleId]);
-
-  return (
-    <>
+return (
+<>
    
 {errorMessage && <p className="error">{errorMessage}</p>}
 {successMessage && <p className="success">{successMessage}</p>}
