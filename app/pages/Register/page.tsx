f2b5608bf5e63461d62ceec18fe5a4b26f25b51f
Phillip Bailey <ztron500@gmail.com>
2024-04-08 20:24:45 -0400

    Update page.tsx

--- app/pages/Register/page.tsx
+++ app/pages/Register/page.tsx
@@ -5,7 +5,7 @@ import Navbar from '@/app/components/navbar';
 import Footer from '@/app/components/footer';
 import RegisterForm from './RegisterForm';
 
-export const meta: Metadata = {
+export const metadata: Metadata = {
     title: 'iTruth News - Register',
     description: 'Sign up for iTruth News - your reliable source for breaking news updates and the latest headlines. Stay informed with in-depth analyses, timely coverage, and accurate insights across a wide range of topics.',
     keywords: 'iTruth News, register, breaking news, latest news headlines, news updates, in-depth analyses, timely coverage, accurate insights'
