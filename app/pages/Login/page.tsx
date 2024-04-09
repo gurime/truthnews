bf7471aceea103fc51f5bffda2e358025ded4cd7
Phillip Bailey <ztron500@gmail.com>
2024-04-08 20:22:10 -0400

    Update page.tsx

--- app/pages/Login/page.tsx
+++ app/pages/Login/page.tsx
@@ -5,7 +5,7 @@ import AdminHeader from '@/app/components/AdminHeader';
 import Navbar from '@/app/components/navbar';
 import Footer from '@/app/components/footer';
 
-export const meta: Metadata = {
+export const Loginmeta: Metadata = {
 title: 'iTruth News - Login',
 description: 'Login to iTruth News - your reliable source for breaking news updates and the latest headlines. Stay informed with in-depth analyses, timely coverage, and accurate insights across a wide range of topics.',
 keywords: 'iTruth News, login, breaking news, latest news headlines, news updates, in-depth analyses, timely coverage, accurate insights'
