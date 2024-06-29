cc4370b71fe50b05663ca8d5ea046ead36740c2d
Phillip Bailey <ztron500@gmail.com>
2024-06-29 15:39:05 -0400

    Update AdminForm.tsx

--- app/pages/Admin/AdminForm.tsx
+++ app/pages/Admin/AdminForm.tsx
@@ -69,70 +69,124 @@ const acceptedCollections = [
 'Space Technology',
 //Tech page stops here
 'Featured Politics',
-'Politics',
+'Headline Politics',
+'Opinion Politics',
+'Local Politics',
+'National Politics',
+'International Politics',
+'Election Politics',
+'Economic Politics',
+'Enviromental Politics',
+'Social Politics',
+'Education Politics',
 //Politics page stops here
 'Featured Opinion',
-'Opinion',
+'Headline Opinion',
+'Guest Opinion',
+'Cultural Opinion',
+'Technology Opinion',
+'Economic Opinion',
+'Enviromental Opinion',
+'Social Opinion',
+'Educational Opinion',
 //Opinion page stops here
 'Featured Music',
-'Music',
+'Headline Music',
+'Opinion Music',
+'Industry Music',
+'Technology Music',
+'Events Music',
+'Culture Music',
+'Video Music',
+'Interview Music',
 //Music page stops here
 'Featured Sports',
-'Sports',
+'Haedline Sports',
+'Opinion Sports',
+'Featured Sports',
+'Headline Sports',
+'Opinion Sports',
+"Featured Sports",
+"Headline Sports",
+"Opinion Sports",
+"Stats Sports",
+"Fantasy Sports",
+"Gear Sports",
+"Guest Sports",
+"Interview Sports",
+"E-Sports",
 //Sports page stops here
 'Featured Military',
-'Military',
+'Headline Military',
+'Opinion Military',
 //Military page stops here
 'Featured Crime',
-'Crime',
+'Headline Crime',
+'Opinion Crime',
 //Crime page stops here
 'Featured Economy',
-'Economy',
+'Headline Economy',
+'Opinion Economy',
 //Economy page stops here
 'Featured Immigration',
-'Immigration',
+'Headline Immigration',
+'Opinion Immigration',
 //Immigration page stops here
 'Featured Business',
-'Business',
+'Headline Business',
+'Opinion Business',
 //Business page stops here
 'Featured Video Games',
-'Video Games',
+'Headline Video Games',
+'Opinion Video Games',
 //Video Games page stops here
 'Featured Entertainment',
-'Entertainment',
+'Headline Entertainment',
+'Opinion Entertainment',
 //Entertainment page stops here
 'Featured Fashion',
-'Fashion',
+'Headline Fashion',
+'Opinion Fashion',
 //Fashion page stops here
 'Featured Education',
-'Education',
+'Headline Education',
+'Opinion Education',
 //Education page stops here
 'Featured U.N. (United Nations)',
-'U.N. (United Nations)',
+'Headline U.N.',
+'Opinion U.N.',
 //U.N. page stops here
 'Featured Terrorism',
-'Terrorism',
+'Headline Terrorism',
+'Opinion Terrorism',
 //Terrism page stops here
 'Featured World Economy',
-'World Economy',
+'Headline World Economy',
+'Opinion World Economy',
 //Economy page stops here
 'Featured Scandals',
-'Scandals',
+'Headline Scandals',
+'Opinion Scandals',
 //Scandals page stops here
 'Featured Mexico',
-'Mexico',
+'Headline Mexico',
+'Opinion Mexico',
 //Mexico page stops here
 'Featured South America',
-'South America',
+'Headline South America',
+'Opinion South America',
 //South America page stops here
 'Featured Europe',
-'Europe',
+'Headline Europe',
+'Opinion Europe',
 //Europe page stops here
 'Featured Asia',
-'Asia',
+'Headline Asia',
+'Opinion Asia',
 //Asia page stops here
 'Featured Africa',
-'Africa'
+'Headline Africa',
+'Opinion Africa',
 //Africa page stops here
 ];
 const [catorgory, setCatorgory] = useState<string>('');
@@ -274,91 +328,142 @@ case 'Space Technology':
 router.push('/pages/Technology');
 break;
 case 'Featured Politics':
-case 'Politics':
+case 'Headline Politics':
+case 'Opinion Politics':
+case 'Local Politics':
+case 'National Politics':
+case 'International Politics':
+case 'Election Politics':
+case 'Economic Politics':
+case 'Environmental Politics':
+case 'Social Politics':
+case 'Education Politics':
 router.push('/pages/Politics');
 break;
 case 'Featured Opinion':
-case 'Opinion':
+case 'Headline Opinion':
+case 'Guest Opinion':
+case 'Cultural Opinion':
+case 'Technology Opinion':
+case 'Economic Opinion':
+case 'Social Opinion':
+case 'Education Opinion':
+case 'Enviromental Opinion':
 router.push('/pages/Opinion');
 break;
 case 'Featured Music':
-case 'Music':
+case 'Headline Music':
+case 'Opinion Music':
+case 'Industry Music':
+case 'Technology Music':
+case 'Events Music':
+case 'Culture Music':
+case 'Video Music':
+case 'Interview Music':
 router.push('/pages/Music');
 break;
 case 'Featured Sports':
-case 'Sports':
+case 'Headline Sports':
+case 'Opinion Sports':
+case "Featured Sports":
+case "Headline Sports":
+case "Opinion Sports":
+case "Stats Sports":
+case "Fantasy Sports":
+case "Gear Sports":
+case "Guest Sports":
+case "Interview Sports":
+case "E-Sports":
 router.push('/pages/Sports');
 break;
 case 'Featured Military':
-case 'Military':
+case 'Headline Military':
+case 'Opinion Military':
 router.push('/pages/Military');
 break;
 case 'Featured Crime':
-case 'Crime':
+case 'Headline Crime':
+case 'Opinion Crime':
 router.push('/pages/Crime');
 break;
 case 'Featured Economy':
-case 'Economy':
+case 'Headline Economy':
+case 'Opinion Economy':
 router.push('/pages/Economy');
 break;
 case 'Featured Immigration':
-case 'Immigration':
+case 'Headline Immigration':
+case 'Opinion Immigration':
 router.push('/pages/Immigration');
 break;
 case 'Featured Business':
-case 'Business':
+case 'Headline Business':
+case 'Opinion Business':
 router.push('/pages/Business');
 break;
 case 'Featured Video Games':
-case 'Video Games':
+case 'Headline Video Games':
+case 'Opinion Video Games':
 router.push('/pages/VideoGames');
 break;
 case 'Featured Entertainment':
-case 'Entertainment':
+case 'Headline Entertainment':
+case 'Opinion Entertainment':
 router.push('/pages/Entertainment');
 break;
 case 'Featured Fashion':
-case 'Fashion':
+case 'Headline Fashion':
+case 'Opinion Fashion':
 router.push('/pages/Fashion');
 break;
 case 'Featured Education':
-case 'Education':
+case 'Headline Education':
+case 'Opinion Education':
 router.push('/pages/Education');
 break;
 case 'Featured U.N. (United Nations)':
-case 'U.N.':
+case 'Headline U.N.':
+case 'Opinion U.N.':
 router.push('/pages/UN');
 break;
 case 'Featured Terrorism':
-case 'Terrorism':
+case 'Headline Terrorism':
+case 'Opinion Terrorism':
 router.push('/pages/Terrorism');
 break;
 case 'Featured Economy':
-case 'World Economy':
+case 'Headline World Economy':
+case 'Opinion World Economy':
 router.push('/pages/WorldEconomy');
 break;
 case 'Featured Scandals':
-case 'Scandals':
+case 'Headline Scandals':
+case 'Opinion Scandals':
 router.push('/pages/Scandals');
 break;
 case 'Featured Mexico':
-case 'Mexico':
+case 'Headline Mexico':
+case 'Opinion Mexico':
 router.push('/pages/Mexico');
 break;
 case 'Featured South America':
-case 'South America':
+case 'Headline South America':
+case 'Opinion South America':
 router.push('/pages/SouthAmerica');
 break;
 case 'Featured Europe':
-case 'Europe':
+case 'Headline Europe':
+case 'Opinion Europe':
 router.push('/pages/Europe');
 break;
 case 'Featured Asia':
-case 'Asia':
+case 'Headline Asia':
+case 'Opinion Asia':
 router.push('/pages/Asia');
 break;
 case 'Featured Africa':
-case 'Africa':
+case 'Headline Africa':
+case 'Opinion Africa':
 router.push('/pages/Africa');
 break;
 default:
@@ -417,6 +522,7 @@ className='billingselect'>
 <option value="Europe Dashboard">Europe Dashboard</option>
 <option value="Asia Dashboard">Asia Dashboard</option>
 <option value="Africa Dashboard">Africa Dashboard</option>
+{/* home page stops here */}
 <option value="Featured Technology">Featured Technology</option>
 <option value="Headline Technology"> Technology</option>
 <option value="Opinion Technology">Opinion Technology</option>
@@ -430,50 +536,121 @@ className='billingselect'>
 <option value="Blockchain Technology">Blockchain Technology</option>
 <option value="Health Technology">Health Technology</option>
 <option value="Space Technology">Space Technology</option>
+{/* technology page stops here */}
 <option value="Featured Politics">Featured Politics</option>
-<option value=" Politics"> Politics</option>
+<option value=" Headline Politics"> Headline Politics</option>
+<option value='Opinion Politics'>Opinion Politics</option>
+<option value="Local Politics">Local Politics</option>
+<option value="National Politics">National Politics</option>
+<option value="International Politics">International Politics</option>
+<option value="Election Politics">Election Politics</option>
+<option value="Economic Politics">Economic Politics</option>
+<option value="Environmental Politics">Environmental Politics</option>
+<option value="Social Politics">Social Politics</option>
+<option value="Education Politics">Education Politics</option>
+{/* politics page stops here */}
 <option value="Featured Opinion">Featured Opinion</option>
-<option value=" Opinion"> Opinion</option>
+<option value="Headline Opinion">Headline Opinion</option>
+<option value="Guest Opinion">Guest Opinion</option>
+<option value="Cultural Opinion">Cultural Opinion</option>
+<option value="Technology Opinion">Technology Opinion</option>
+<option value="Economic Opinion">Economic Opinion</option>
+<option value="Environmental Opinion">Environmental Opinion</option>
+<option value="Social Opinion">Social Opinion</option>
+<option value="Educational Opinion">Educational Opinion</option>
+{/* Opnion page stops here */}
 <option value="Featured Music">Featured Music</option>
-<option value="Music">Music</option>
+<option value="Headline Music">Headline Music</option>
+<option value="Opinion Music">Opinion Music</option>
+<option value="Industry Music">Industry Music</option>
+<option value="Technology Music">Technology Music</option>
+<option value="Events Music">Events Music</option>
+<option value="Culture Music">Culture Music</option>
+<option value="Video Music">Music Videos</option>
+<option value="Interview Music">Music Interviews</option>
+{/* music page stops here */}
 <option value="Featured Sports">Featured Sports</option>
-<option value="Sports">Sports</option>
+<option value="Headline Sports">Headline Sports</option>
+<option value="Opinion Sports">Opinion Sports</option>
+<option value="Stats Sports">Stats Sports</option>
+<option value="Fantasy Sports">Fantasy Sports</option>
+<option value="Gear Sports">Sports Gear</option>
+<option value="Guest Sports">Guest Sports</option>
+<option value="Interview Sports">Interview Sports</option>
+<option value="E-Sports">E-Sports</option>
+{/* sports page stops here */}
 <option value="Featured Military">Featured Military</option>
-<option value="Military"> Military</option>
+<option value="Headline Military"> Headline Military</option>
+<option value="Opinion Military"> Opinion Military</option>
+{/* military page stops here */}
 <option value="Featured Crime">Featured Crime</option>
-<option value=" Crime"> Crime</option>
+<option value=" Headline Crime">Headline Crime</option>
+<option value=" Opinion Crime">Opinion Crime</option>
+{/* crime page stops here */}
 <option value="Featured Economy">Featured Economy</option>
-<option value=" Economy"> Economy</option>
+<option value=" Headline Economy"> Headline Economy</option>
+<option value=" Opinion Economy"> Opinion Economy</option>
+{/* economy page stips here */}
 <option value="Featured Immigration">Featured Immigration</option>
-<option value=" Immigration"> Immigration</option>
+<option value=" Headline Immigration"> Headline Immigration</option>
+<option value=" Opinion Immigration"> Opinion Immigration</option>
+{/* Immigration page stops here */}
 <option value="Featured Business">Featured Business</option>
-<option value="Business">Business</option>
+<option value="Headline Business">Headline Business</option>
+<option value="Opinion Business">Opinion Business</option>
+{/* Business page stops here */}
 <option value="Featured Video Games">Featured Video Games</option>
-<option value="Video Games">Video Games</option>
+<option value="Headline Video Games">Headline Video Games</option>
+<option value="Opinion Video Games">Opinion Video Games</option>
+{/* Video Games Page stops here */}
 <option value="Featured Entertainment">Featured Entertainment</option>
-<option value="Entertainment">Entertainment</option>
+<option value="Headline Entertainment">Headline Entertainment</option>
+<option value="Opinion Entertainment">Opinion Entertainment</option>
+{/* Entertainment page stops here */}
 <option value="Featured Fashion">Featured Fashion</option>
-<option value="Fashion">Fashion</option>
+<option value="Headline Fashion">Headline Fashion</option>
+<option value="Opinion Fashion">Opinion Fashion</option>
+{/* Fashion page stops here */}
 <option value="Featured Education">Featured Education</option>
-<option value="Education">Education</option>
+<option value="Headline Education">Headline Education</option>
+<option value="Opinion Education">Opinion Education</option>
+{/* Education page stops here */}
 <option value="Featured U.N. (United Nations)">Featured U.N. (United Nations)</option>
-<option value="U.N.">U.N. (United Nations)</option>
+<option value="Headline U.N.">Headline U.N. (United Nations)</option>
+<option value="Opinion U.N.">Opinion U.N. (United Nations)</option>
+{/* UN page stops here */}
 <option value="Featured Terrorism">Featured Terrorism</option>
-<option value="Terrorism">Terrorism</option>
+<option value="Headline Terrorism">Headline Terrorism</option>
+<option value="Opinion Terrorism">Opinion Terrorism</option>
+{/* terrorism page stops here */}
 <option value="Featured World Economy">Featured World Economy</option>
-<option value="World Economy">World Economy</option>
+<option value="Headline World Economy">Headline World Economy</option>
+<option value="Opinion World Economy">Opinion World Economy</option>
+{/* world economy page stops here */}
 <option value="Featured Scandals">Featured Scandals</option>
-<option value="Scandals">Scandals</option>
+<option value="Headline Scandals">Headline Scandals</option>
+<option value="Opinion Scandals">Opinion Scandals</option>
+{/* scandals page stops here */}
 <option value="Featured Mexico">Featured Mexico</option>
-<option value="Mexico">Mexico</option>
+<option value="Headline Mexico">Headline Mexico</option>
+<option value="Opinion Mexico">Opinion Mexico</option>
+{/* mexico page stops here */}
 <option value="Featured South America">Featured South America</option>
-<option value="South America">South America</option>
+<option value="Headline South America">Headline South America</option>
+<option value="Opinion South America">Opinion South America</option>
+{/* south america page stops here */}
 <option value="Featured Europe">Featured Europe</option>
-<option value="Europe">Europe</option>
+<option value="Headline Europe">Headline Europe</option>
+<option value="Opinion Europe">Opinion Europe</option>
+{/* europe page stops here */}
 <option value="Featured Asia">Featured Asia</option>
-<option value="Asia">Asia</option>
+<option value="Headline Asia">Headline Asia</option>
+<option value="Opinion Asia">Opinion Asia</option>
+{/* asia page stops here */}
 <option value="Featured Africa">Featured Africa</option>
-<option value="Africa">Africa</option>
+<option value="Headline Africa">Headline Africa</option>
+<option value="Opinion Africa">Opinion Africa</option>
+{/* africa page stops here */}
 </select>
 </div>
 
