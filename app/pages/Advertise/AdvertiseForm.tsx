979af5465922af010fd2ba555840aa912e30217b
Phillip Bailey <ztron500@gmail.com>
2024-07-09 17:48:07 -0400

    Update AdvertiseForm.tsx

--- app/pages/Advertise/AdvertiseForm.tsx
+++ app/pages/Advertise/AdvertiseForm.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import adimg from '../../images/adimg.jpeg'
 import Image from 'next/image'
-import { FaCreditCard } from 'react-icons/fa'
-import { GrPaypal } from 'react-icons/gr'
+
 
 export default function AdvertiseForm() {
 return (
@@ -62,11 +61,7 @@ display:'grid',
 placeContent:'center'}}>
 
 <div className='formbox'>
-<label  htmlFor='fname'>Firstname</label>
-<input  type='text'  id='fname'/>
 
-<label  htmlFor='lname'>Lastname</label>
-<input type='text'  id='lname'/>
 
 
 
@@ -75,53 +70,8 @@ placeContent:'center'}}>
 <p style={{fontSize:'20px',fontWeight:'600'}}>Payment Method</p>
 <p >🔒Secure Transaction</p>
 </div>
-<fieldset className='form-radio-group' >
 
-<label className='form-radio-box'  htmlFor='1'>
-<input type='checkbox'
-style={{
-display:'flex',
-margin:'auto',
-width:'24px',
-backgroundColor:'transparent',
-border:'solid 1px currentcolor',
-cursor:'pointer',}}
-name='age'
-id='1'/>
-
-<div className='payment-method-box'>
-<div>Credit/Debit Card</div>
-<FaCreditCard />
-
-</div>
-</label>
-
-<label className='form-radio-box'  htmlFor='2'>
-<input type='checkbox'
-style={{
-display:'flex',
-margin:'auto',
-width:'24px',
-backgroundColor:'transparent',
-border:'solid 1px currentcolor',
-cursor:'pointer',}}
-name='age'
-id='2'/>
-
-<div className='payment-method-box'>
-<div >Paypal</div>
-<GrPaypal />
-
-</div>
-</label>
-
-</fieldset>
 <button> $7 per month</button>
-<p style={{
-textAlign:'center',
-
-
-}}>Cancel Anytime</p>
 </div>
 </div>
 </div>
